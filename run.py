e15b11f7592694d85b1954896eb1854342528f65
brgangi <benjamin.gangi@student.unimelb.edu.au>
2024-02-08 21:15:02 +1100

    started forming website template

--- run.py
+++ run.py
@@ -1,12 +1,38 @@
 from game import Game
 from constants import NOT_FINISHED
+from flask import Flask, render_template, jsonify
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+SYMBOL = 'X'  
+
+@app.route('/button_click', methods=['POST'])
+def button_click():
+    global SYMBOL 
+    if SYMBOL == 'X':
+        SYMBOL = 'O'
+    else:
+        SYMBOL = 'X'
+    return jsonify(SYMBOL)
+
+@app.route('/main', methods=['POST'])
 def main():
+    print("here")
     game = Game()
 
     while game.board.check_end() == NOT_FINISHED:
         game.get_move()
     game.get_winner()
 
-if __name__ == "__main__":
-    main()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
